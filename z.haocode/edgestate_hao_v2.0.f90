4fe94ab8ec1359246c50b9f45297c47e7b69d1a9
Hao Wang <wanghao90515@163.com>
2023-07-04 19:26:05 +0200

    test barrier

--- z.haocode/edgestate_hao_v2.0.f90
+++ z.haocode/edgestate_hao_v2.0.f90
@@ -179,9 +179,13 @@ program hao_edgestates
     write(*,*) "ndiffatom = ",ndiffatom,irank 
 
     call mpi_bcast(fourdim,1,MPI_INTEGER,0,mpi_comm_world,ierr)
-    write(*,*) "fourdim=",fourdim,irank 
-    call mpi_bcast(fourdir,size(fourdir),MPI_INTEGER,0,mpi_comm_world,ierr)
+    write(*,*) "fourdim=",fourdim,irank
+
+    call MPI_Barrier(mpi_comm_world, ierr)
+    
+    call mpi_bcast(fourdir,fourdim,MPI_INTEGER,0,mpi_comm_world,ierr)
     write(*,*) "fourdir=",fourdir,irank 
+    
     call mpi_bcast(layerdir,1,MPI_INTEGER,0,mpi_comm_world,ierr)
     write(*,*) "layerdir =",layerdir,irank 
 
