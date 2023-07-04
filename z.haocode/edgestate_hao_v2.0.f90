2e7b48225e59f1d27f7134b654fd6aedfae10755
Hao Wang <wanghao90515@163.com>
2023-07-04 10:22:57 +0200

    test1

--- z.haocode/edgestate_hao_v2.0.f90
+++ z.haocode/edgestate_hao_v2.0.f90
@@ -324,9 +324,9 @@ program hao_edgestates
         call mpi_bcast(fourdirection,1,MPI_INTEGER,0,mpi_comm_world,ierr)
         call mpi_bcast(fourdim,1,MPI_INTEGER,0,mpi_comm_world,ierr)
         call mpi_bcast(numkpts,1,MPI_INTEGER,0,mpi_comm_world,ierr)
-        call mpi_bcast(fourHamilton,size(fourHamilton),MPI_DOUBLE_PRECISION,0,mpi_comm_world,ierr)
-        call mpi_bcast(hamiltonian,size(hamiltonian),MPI_DOUBLE_PRECISION,0,mpi_comm_world,ierr)
-        call mpi_bcast(wannierfunctioninham,size(wannierfunctioninham),MPI_DOUBLE_PRECISION,0,mpi_comm_world,ierr)
+      !  call mpi_bcast(fourHamilton,size(fourHamilton),MPI_DOUBLE_PRECISION,0,mpi_comm_world,ierr)
+      !  call mpi_bcast(hamiltonian,size(hamiltonian),MPI_DOUBLE_PRECISION,0,mpi_comm_world,ierr)
+      !  call mpi_bcast(wannierfunctioninham,size(wannierfunctioninham),MPI_DOUBLE_PRECISION,0,mpi_comm_world,ierr)
     if(irank.eq.0)then 
         do ik=1,numkpts
             k(ik) = ik*3*pi/numkpts
