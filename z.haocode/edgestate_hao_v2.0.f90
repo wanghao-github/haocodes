29350484fe748a90b2aef8e74b573fb2dded0b69
Hao Wang <wanghao90515@163.com>
2023-07-06 11:56:21 +0200

    allreduce 到循环外面

--- z.haocode/edgestate_hao_v2.0.f90
+++ z.haocode/edgestate_hao_v2.0.f90
@@ -499,7 +499,7 @@ call MPI_Barrier(mpi_comm_world, ierr)
      write(*,*) "zheevx no problem, irank =" ,irank
 
        eigvals_per_k(ik, :) = eigvals(:)
-       call MPI_ALLREDUCE(eigvals_per_k,eigvals_per_k_mpi,size(eigvals_per_k),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
+       
 
        write(*,*) "write eigvals OK, irank =" ,irank
 
@@ -544,10 +544,11 @@ call MPI_Barrier(mpi_comm_world, ierr)
             write(*,*) "dos_bulk ok"
         enddo ! j
 !
+    call MPI_ALLREDUCE(eigvals_per_k,eigvals_per_k_mpi,size(eigvals_per_k),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
 
-  !  call mpi_allreduce(dos_l, dos_l_mpi, size(dos_l),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
-  !  call mpi_allreduce(dos_r, dos_r_mpi, size(dos_r),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
-  !  call mpi_allreduce(dos_bulk, dos_bulk_mpi, size(dos_bulk),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
+!    call mpi_allreduce(dos_l, dos_l_mpi, size(dos_l),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
+!    call mpi_allreduce(dos_r, dos_r_mpi, size(dos_r),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
+!    call mpi_allreduce(dos_bulk, dos_bulk_mpi, size(dos_bulk),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
     
     write(*,*) "AllREDUCE DONE"
 
