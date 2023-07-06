e99aa84793c50d8f89381ea011bfa456f6d3372f
Hao Wang <wanghao90515@163.com>
2023-07-06 10:42:12 +0200

    from reduce to allreduce

--- z.haocode/edgestate_hao_v2.0.f90
+++ z.haocode/edgestate_hao_v2.0.f90
@@ -538,9 +538,9 @@ call MPI_Barrier(mpi_comm_world, ierr)
         enddo ! j
 
 
-    call mpi_reduce(dos_l, dos_l_mpi, size(dos_l),MPI_DOUBLE_PRECISION,MPI_SUM,0,mpi_comm_world,ierr)
-    call mpi_reduce(dos_r, dos_r_mpi, size(dos_r),MPI_DOUBLE_PRECISION,MPI_SUM,0,mpi_comm_world,ierr)
-    call mpi_reduce(dos_bulk, dos_bulk_mpi, size(dos_bulk),MPI_DOUBLE_PRECISION,MPI_SUM,0,mpi_comm_world,ierr)
+    call mpi_allreduce(dos_l, dos_l_mpi, size(dos_l),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
+    call mpi_allreduce(dos_r, dos_r_mpi, size(dos_r),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
+    call mpi_allreduce(dos_bulk, dos_bulk_mpi, size(dos_bulk),MPI_DOUBLE_PRECISION,MPI_SUM,mpi_comm_world,ierr)
     
     write(*,*) "AllREDUCE DONE"
 
